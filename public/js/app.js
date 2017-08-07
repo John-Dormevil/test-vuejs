d29451e1b6be9e78d052aede1939c42680a06189
John-Weetaker <jjdormevil@gmail.com>
2017-08-07 14:32:52 +0200

    mix laravel and vuejs to obtain app with ajax request

---
+++ public/js/app.js
@@ -0,0 +1,16 @@
+window.http = axios;
+
+
+
+new Vue({
+    el: '#app',
+
+    mounted () {
+        http.get('/skills').then(response => {this.skills = response.data});
+    },
+    data () {
+        return {
+            skills:  []
+        }
+    }
+})
