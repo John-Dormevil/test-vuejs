d2db67555ddb17e8057e1213448c239478090482
John-Weetaker <jjdormevil@gmail.com>
2017-09-30 16:30:31 +0200

    adding list technique eg:

--- main.js
+++ main.js
@@ -1,57 +1,13 @@
-Vue.component('tabs', {
-  template:
-  `
-  <div>
-    <div class="tabs">
-      <ul>
-        <li v-for="tab in tabs" :class= "{'is-active' : tab.isActive }">
-          <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
-        </li>
-      </ul>
-    </div>
-
-    <div class='tabs-details'>
-      <slot></slot>
-    </div>
-  </div>
-  `,
-  data() {
-    return { 
-      tabs: []
-    }
-  },
-  created() {
-    this.tabs = this.$children;
+var app = new Vue({
+  el: '#root',
+  data:{
+    names: ['joe', 'marie', 'jane', 'jack'],
+    newName: ''
   },
   methods: {
-    selectTab(selectTab) {
-      this.tabs.forEach(tab => {
-        tab.isActive = tab.name == selectTab.name ? true : false;
-      });
+    addName: function () {
+      this.names.push(this.newName);
+      this.newName = '';
     }
   }
-});
-
-Vue.component('tab', {
-  template: `<div v-show="isActive"><slot></slot></div>`,
-  data() {
-    return {
-      isActive: this.selected,
-      href: "#"+this.name.toLowerCase().replace(/ /g, '-')
-    }
-  },
-  props: {
-    name: {
-      type: String,
-      required: true,
-      default: 'title name'
-    },
-    selected: {
-      default: false
-    }
-  }
-});
-
-new Vue({
-  el: '#root'
 });
